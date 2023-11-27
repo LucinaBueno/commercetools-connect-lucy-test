b3cad258731ee913614a213c14bbfb0ae30674e6
LucinaBueno <lucina.bueno@jwmsolutions.com>
2023-11-27 13:38:53 -0600

    Logger added for testing purposes

--- event/src/routes/event.route.ts
+++ event/src/routes/event.route.ts
@@ -6,6 +6,7 @@ const eventRouter: Router = Router();
 
 eventRouter.post('/', async (req, res) => {
   logger.info('Event message received');
+  logger.info(JSON.stringify(req.body));
   res.status(200);
   res.send();
 });
