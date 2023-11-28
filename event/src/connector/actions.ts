3979e35742864948567151120c7ffba7264a9984
LucinaBueno <lucina.bueno@jwmsolutions.com>
2023-11-28 12:08:04 -0600

    - Se agregó el atributo required en todos los datos que se deben proporcionar al deployar el conector. - Se configuró la creación de una suscripción para 'product'->'ProductPublished' durante el post deploy del conector. - Se comentó el codigo que crea una suscripción para el evento de creación de clientes y se agregó la línea para eliminar la suscripción ya que no la vamos a necesitar.

--- event/src/connector/actions.ts
+++ event/src/connector/actions.ts
@@ -1,7 +1,58 @@
 import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 
-const CUSTOMER_CREATE_SUBSCRIPTION_KEY =
-  'myconnector-customerCreateSubscription';
+const CUSTOMER_CREATE_SUBSCRIPTION_KEY = 'myconnector-customerCreateSubscription';
+const PRODUCT_PUBLISHED_SUBSCRIPTION_KEY = 'myconnector-productPublishedSubscription';
+
+export async function createProductPublishedSubscription(
+apiRoot: ByProjectKeyRequestBuilder,
+  topicName: string,
+  projectId: string
+): Promise<void> {
+  const {
+    body: { results: subscriptions },
+  } = await apiRoot
+    .subscriptions()
+    .get({
+      queryArgs: {
+        where: `key = "${PRODUCT_PUBLISHED_SUBSCRIPTION_KEY}"`,
+      },
+    })
+    .execute();
+
+  if (subscriptions.length > 0) {
+    const subscription = subscriptions[0];
+
+    await apiRoot
+      .subscriptions()
+      .withKey({ key: PRODUCT_PUBLISHED_SUBSCRIPTION_KEY })
+      .delete({
+        queryArgs: {
+          version: subscription.version,
+        },
+      })
+      .execute();
+  }
+
+  await apiRoot
+    .subscriptions()
+    .post({
+      body: {
+        key: PRODUCT_PUBLISHED_SUBSCRIPTION_KEY,
+        destination: {
+          type: 'GoogleCloudPubSub',
+          topic: topicName,
+          projectId,
+        },
+        messages: [
+          {
+            resourceTypeId: 'product',
+            types: ['ProductPublished'],
+          },
+        ],
+      },
+    })
+    .execute();
+}
 
 export async function createCustomerCreateSubscription(
   apiRoot: ByProjectKeyRequestBuilder,
