9d20530ba356aa97c8fdb0a18ae3f09f681d9965
LucinaBueno <lucina.bueno@jwmsolutions.com>
2023-11-29 13:01:47 -0600

    - Logger variables de ambiente

--- event/src/routes/event.route.ts
+++ event/src/routes/event.route.ts
@@ -7,8 +7,14 @@ const eventRouter: Router = Router();
 eventRouter.post('/', async (req, res) => {
   logger.info('Event message received...');
   logger.info('-> Event request:', req);
-  logger.info('-> OG_CLIENT_ID:', process.env.OG_CLIENT_ID);
-  logger.info('-> OG_CLIENT_SECRET:', process.env.OG_CLIENT_SECRET);
+
+  const ctpProjectKey = process.env.CTP_PROJECT_KEY as string;
+  const ogClientId = process.env.OG_CLIENT_ID as string;
+  const ogClientSecret = process.env.OG_CLIENT_SECRET as string;
+
+  logger.info('-> CTP_PROJECT_KEY:' + ctpProjectKey);
+  logger.info('-> OG_CLIENT_ID:' + ogClientId);
+  logger.info('-> OG_CLIENT_SECRET:' + ogClientSecret);
   res.status(200);
   res.send();
 });
