8b0689404a8924857e12feb0cc1ea486fcd114e0
LucinaBueno <lucina.bueno@jwmsolutions.com>
2023-11-29 12:12:30 -0600

    - Prueba de credenciales de Ordergroove API

--- event/src/routes/event.route.ts
+++ event/src/routes/event.route.ts
@@ -7,6 +7,8 @@ const eventRouter: Router = Router();
 eventRouter.post('/', async (req, res) => {
   logger.info('Event message received...');
   logger.info('-> Event request:', req);
+  logger.info('-> OG_CLIENT_ID:', process.env.OG_CLIENT_ID);
+  logger.info('-> OG_CLIENT_SECRET:', process.env.OG_CLIENT_SECRET);
   res.status(200);
   res.send();
 });
