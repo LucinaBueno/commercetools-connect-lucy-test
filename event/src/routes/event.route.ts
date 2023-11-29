646d0f910056fafeb7f7619c4a56a93b4e533fe5
LucinaBueno <lucina.bueno@jwmsolutions.com>
2023-11-29 16:03:38 -0600

    - Se agrega procesamiento de la data recibida del evento y se realiza el decode para ver la información en claro.

--- event/src/routes/event.route.ts
+++ event/src/routes/event.route.ts
@@ -5,7 +5,7 @@ import { logger } from '../utils/logger.utils';
 const eventRouter: Router = Router();
 
 eventRouter.post('/', async (req, res) => {
-  logger.info('Event message received...');
+  //logger.info('Event message received...');
   logger.info('-> Event request:', req);
 
   const ctpProjectKey = process.env.CTP_PROJECT_KEY as string;
@@ -15,6 +15,22 @@ eventRouter.post('/', async (req, res) => {
   logger.info('-> CTP_PROJECT_KEY:' + ctpProjectKey);
   logger.info('-> OG_CLIENT_ID:' + ogClientId);
   logger.info('-> OG_CLIENT_SECRET:' + ogClientSecret);
+
+
+  const { body } = req;
+
+  if (body.message !== undefined && body.message.data !== undefined) {
+    const encodedData = body.message.data;
+
+    let buff = new Buffer(encodedData, 'base64');
+    let decodedData = buff.toString('ascii');
+
+    console.log('-> Decoded event data:', decodedData);
+
+  } else {
+    console.log('-> There is not data for this event!');
+  }
+
   res.status(200);
   res.send();
 });
